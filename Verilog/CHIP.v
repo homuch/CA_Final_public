b7a7e80f7a0c6b364e23fff9314e9f6c67b6643e
homuch <homu777@gmail.com>
2022-12-26 18:00:28 +0800

    ts

--- Verilog/CHIP.v
+++ Verilog/CHIP.v
@@ -26,20 +26,24 @@ module CHIP(clk,
     // Do not modify this part!!!            //
     // Exception: You may change wire to reg //
     reg    [31:0] PC          ;              //
-    wire   [31:0] PC_nxt      ;              //
+    reg   [31:0] PC_nxt      ;              //
     wire          regWrite    ;              //
     wire   [ 4:0] rs1, rs2, rd;              //
     wire   [31:0] rs1_data    ;              //
     wire   [31:0] rs2_data    ;              //
-    wire   [31:0] rd_data     ;              //
+    reg   [31:0] rd_data     ;              //
     //---------------------------------------//
 
     // Todo: other wire/reg
 
     reg [4:0] state, state_nxt;
+    reg [2:0] sub_state, sub_state_nxt;
+    reg readInstr,nextReadInstr, updatePC;
+    wire branch;
+    reg regShouldWrite;
 
     parameter IDLE = 5'd0;
-    parameter AUPIC  = 5'd1;
+    parameter AUIPC  = 5'd1;
     parameter JAL  = 5'd2;
     parameter JALR = 5'd3;
     parameter BEQ = 5'd4;
@@ -51,6 +55,12 @@ module CHIP(clk,
     parameter SUB  = 5'd10;
     parameter XOR  = 5'd11;
     parameter MUL  = 5'd12;
+
+    parameter ID = 3'd0;
+    parameter EX = 3'd1;
+    parameter MEM = 3'd2;
+    parameter WB = 3'd3;
+    parameter BUFFER = 3'd4;
 
     //==== Submodule Connection ===================
     //---------------------------------------//
@@ -71,59 +81,167 @@ module CHIP(clk,
 
     //==== Combinational Part =====================
 
-    assign rs1=mem_rdata_I[19:15];
-    assign rs2=mem_rdata_I[24:20];
-    assign rd=mem_rdata_I[11:7];
+    assign rs1 = mem_rdata_I[19:15];
+    assign rs2 = mem_rdata_I[24:20];
+    assign rd = mem_rdata_I[11:7];
+    assign mem_addr_I = PC;
+    assign mem_addr_D = (state == LW)? rs1_data + mem_rdata_I[31:20]:rs1_data + {mem_rdata_I[31:25],mem_rdata_I[11:7]};
+    assign mem_wen_D = (state == SW && sub_state==EX)? 1'd1:1'd0;
+    assign mem_wdata_D = rs2_data;
+    assign regWrite = regShouldWrite&&(sub_state==WB);
 
+    assign branch = (state==JAL||state==JALR||state==BEQ);
     // Todo: any combinational/sequential circuit
 
     // decode instruction
     always @(*)begin
-        case mem_rdata_I[6:0]
-            7'b0010011:begin//imm
-                case mem_rdata_I[14:12]:
-                    3'b000:begin//addi
-                        state_nxt=ADDI;
+        if(!readInstr)begin
+            state_nxt = state;
+            nextReadInstr = 1'd0;
+        end
+        else begin
+            nextReadInstr=1'd0;
+            sub_state_nxt = EX;
+            case (mem_rdata_I[6:0])
+                7'b0010011:begin//imm
+                    case (mem_rdata_I[14:12])
+                        3'b000:begin//addi
+                            state_nxt = ADDI;
+                        end
+                        3'b010:state_nxt = SLTI;//slti
+                    endcase
+                end
+                7'b0110011:begin//R-type
+                    case ({mem_rdata_I[31:25],mem_rdata_I[14:12]})
+                        10'd0:state_nxt=ADD;//add
+                        10'h100:state_nxt=SUB;//sub
+                        10'h4:state_nxt=XOR;//xor
+                        10'h8:state_nxt=MUL;//mul
+                    endcase
+                end
+                7'b0000011:begin//load
+                    case (mem_rdata_I[14:12])
+                        3'b010:state_nxt=LW;//LW
+                    endcase
+                end
+                7'b0100011:begin//save
+                    case (mem_rdata_I[14:12])
+                        3'b010:state_nxt=SW;//SW
+                    endcase
+                end
+                7'b1100011:begin//branch
+                    case (mem_rdata_I[14:12])
+                        3'b000:state_nxt=BEQ;//beq
+                    endcase
+                end
+
+                7'b0010111:begin//AUIPC
+                    state_nxt=AUIPC;
+                end
+
+                7'b1101111:begin
+                    state_nxt = JAL;
+                end
+
+                7'b1100111:begin
+                    state_nxt = JALR;
+                end
+                default:state_nxt=IDLE;
+            endcase
+        end
+    end
+
+
+    //ALU
+    always @(*)begin
+        case (sub_state)
+            ID:regShouldWrite = 1'd0;
+            EX:begin
+                rd_data = 32'd0;
+                // mem_addr_D=32'd0;
+                sub_state_nxt = WB;
+                updatePC=1'd0;
+                regShouldWrite = 1'd0;
+                // mem_wen_D = 1'd0;
+                case (state)
+                    ADDI:begin
+                        rd_data = mem_rdata_I[31:20]+rs1_data;
                     end
-                    3'b010:state_nxt=SLTI;//slti
+                    // SLTI: rd_data = ($signed(rs1_data)<$signed({20{mem_addr_I[31]},mem_addr_I[31:20]}))? 32'd1:32'd0;
+                    ADD: rd_data = rs1_data+rs2_data;
+                    SUB: rd_data = rs1_data-rs2_data;
+                    XOR: rd_data = rs1_data^rs2_data;
+                    // MUL://mul
+                    LW:begin 
+                        // mem_addr_D = rs1_data + mem_rdata_I[31:20];
+                        // mem_wen_D = 1'd0;
+                    end
+                    SW:begin
+                        // mem_addr_D = rs1_data + {mem_rdata_I[31:25],mem_rdata_I[11:7]};
+                        // mem_wdata_D = rs2_data;
+                        // mem_wen_D = 1'd1;
+                    end
+                    AUIPC:rd_data = {mem_rdata_I[31:12],12'd0} + PC;
+                    // BEQ:sub_state_nxt=ID;
+                    JAL:rd_data = PC+32'd4;
+                    JALR:rd_data = PC+32'd4;
+                    default:begin
+                        sub_state_nxt = ID;
+                        updatePC=1'd1;
+                    end
                 endcase
             end
-            7'b0110011:begin//R-type
-                case {mem_rdata_I[31:25],mem_rdata_I[14:12]}:
-                    10'd0:state_nxt=ADD;//add
-                    10'h100:state_nxt=SUB;//sub
-                    10'h4:state_nxt=XOR;//xor
-                    10'h8:state_nxt=MUL;//mul
+            WB:begin
+                regShouldWrite = 1'd0;
+                sub_state_nxt = BUFFER;
+                case (state)
+                    ADDI:regShouldWrite = 1'd1;
+                    SLTI: regShouldWrite = 1'd1;
+                    ADD: regShouldWrite = 1'd1;
+                    SUB: regShouldWrite = 1'd1;
+                    XOR: regShouldWrite = 1'd1;
+                    // MUL://mul
+                    LW: begin
+                        regShouldWrite = 1'd1;
+                        rd_data = mem_rdata_D;
+                    end
+                    AUIPC:regShouldWrite = 1'd1;
+                    JAL:regShouldWrite = 1'd1;
+                    JALR:regShouldWrite = 1'd1;
                 endcase
             end
-            7'b0000011:begin//load
-                case mem_rdata_I[14:12]:
-                    3b'010:state_nxt=LW;//LW
-                endcase
+            BUFFER:begin
+                sub_state_nxt = ID;
+                updatePC = 1'd1;
             end
-            7'b0100011:begin//save
-                case mem_rdata_I[14:12]:
-                    3b'010:state_nxt=SW;//SW
-                endcase
-            end
-            7'b1100011:begin//branch
-                case mem_rdata_I[14:12]:
-                    3b'000:state_nxt=BEQ;//beq
-                endcase
-            end
-
-            7'b0010111:begin//AUIPC
-                state_nxt=AUPIC;
-            end
-            
-            default:state_nxt=IDLE;
-
         endcase
     end
 
-    //ALU
+    //PC update/branch
+    always @(*)begin
+        PC_nxt = PC;
+        nextReadInstr = 1'd0;
+        if(updatePC)begin
+            nextReadInstr=1'd1;
+            PC_nxt = PC+32'd4;
+            if(branch)begin
+                if(state==JAL)begin
+                    PC_nxt = {mem_rdata_I[31],mem_addr_I[19:12],mem_addr_I[20],mem_addr_I[30:21],1'd0}+PC;
+                end
+                else if(state==JALR)begin
+                    PC_nxt = rs1_data+mem_rdata_I[31:20];
+                end
+                else if(state==BEQ) begin
+                    if(rs1_data==rs2_data)begin
+                        PC_nxt = {mem_rdata_I[12],mem_rdata_I[7],mem_rdata_I[30:25],mem_rdata_I[11:8],1'd0}+PC;
+                    end
+                end            
+        end
+        end
+        updatePC = 1'd0;
+    end
 
-
+    
 
 
     //==== Sequential Part ========================
@@ -131,10 +249,14 @@ module CHIP(clk,
         if (!rst_n) begin
             PC <= 32'h00400000; // Do not modify this value!!!
             state <=IDLE;
+            sub_state <=ID;
+            readInstr <= 1'd1;
         end
         else begin
             PC <= PC_nxt;
-            state <= state_nxt
+            state <= state_nxt;
+            sub_state<=sub_state_nxt;
+            readInstr <=nextReadInstr;
         end
     end
 endmodule
@@ -183,7 +305,7 @@ module reg_file(clk, rst_n, wen, a1, a2, aw, d, q1, q2);
     end
 endmodule
 
-module mulDiv(clk, rst_n, valid, ready, mode, in_A, in_B, out);
-    // Todo: your HW2
+// module mulDiv(clk, rst_n, valid, ready, mode, in_A, in_B, out);
+//     // Todo: your HW2
 
-endmodule
+// endmodule
